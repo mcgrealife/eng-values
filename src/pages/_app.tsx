6fa9d6ca330e959e31c7bc18268bdcfa3429c101
Michael McGreal <mcgrealife@gmail.com>
2023-06-29 10:57:09 -0400

    add error boundary, with sailing quote

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Open_Sans } from 'next/font/google'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 export const openSans = Open_Sans({ subsets: ['latin'] }) // export for pseudo-singleton
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
