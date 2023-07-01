7898deb4041ec487295a521923df404aa647dcd2
Michael McGreal <mcgrealife@gmail.com>
2023-07-01 11:04:45 -0400

    opengraph include twitter

--- src/lib/seo.tsx
+++ src/lib/seo.tsx
@@ -11,6 +11,12 @@ export const openGraphAndMeta = (page: 'home' | 'about') => (
     <title>{titles[page]}</title>
     <meta name='description' content={titles[page]} />
     <meta property='og:image' content={`${baseUrl}/api/v1/og`} />
+    <meta property='twitter:image' content={`${baseUrl}/api/v1/og`} />
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta name='twitter:site' content='@mcgrealife' />
+    <meta name='twitter:title' content={titles[page]} />
+    <meta name='twitter:description' content={titles[page]} />
+    <meta property='og:url' content={baseUrl}></meta>
     <meta property='og:title' content={titles[page]} />
     <meta property='og:description' content={titles[page]} />
     <meta property='twitter:image' content={`${baseUrl}/api/og`} />
