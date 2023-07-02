7eed908d8002e988c4fbfbd363283813a999db77
Michael McGreal <mcgrealife@gmail.com>
2023-07-02 17:13:38 -0400

    fade in scrollToTop button using intersectionObserver

--- src/components/buttons/ScrollToTopButton.tsx
+++ src/components/buttons/ScrollToTopButton.tsx
@@ -1,15 +1,26 @@
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
 import styles from './ScrollToTopButton.module.css'
+import { useEffect, useRef } from 'react'
 
 export default function ScrollToTopButton() {
+  const { inView, observeEl } = useIntersectionObserver({})
+
+  const ref = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    observeEl(ref.current)
+  }, [observeEl])
+
   return (
     <button
+      ref={ref}
       onClick={(e) =>
         e.currentTarget.parentElement?.scrollTo({
           top: 0,
           behavior: 'smooth',
         })
       }
-      className={`${styles.scrollTo} ${false ? styles.fade : ''}`}>
+      className={`${styles.scrollTo} ${inView ? styles.fade : ''}`}>
       Back to top ↑
     </button>
   )
